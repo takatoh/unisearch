ca608e4e4adbe28872e7a7b989bc41d4e68c4565
takatoh <takatoh.m@gmail.com>
2019-12-25 16:13:18 +0900

    search.py: New.

---
+++ search.py
@@ -0,0 +1,37 @@
+from unigram import document
+import json
+import os
+import sys
+
+
+INDEX_DIR = 'indexes'
+DOC_DATA = 'docs.json'
+
+
+string = sys.argv[1]
+
+with open(DOC_DATA, 'r') as f:
+    docs = json.load(f)
+fcount = len(docs)
+
+index_files = list(map(lambda x: os.path.join(INDEX_DIR, x), os.listdir(INDEX_DIR)))
+index = {}
+for file in index_files:
+    c = chr(int(os.path.basename(file).replace('.index', '')))
+    with open(file, 'r') as f:
+        index[c] = document.parse_index(f.read())
+
+m = list(map(lambda x: index[x], list(string)))
+
+for i in range(fcount):
+    doc_id = str(i)
+    if not all(map(lambda x: doc_id in x, m)):
+        continue
+    n = list(map(lambda x: x[doc_id], m))
+    s = set(n[0])
+    for s1 in n[1:]:
+        s = s & set(s1)
+    if len(s) > 0:
+        pos = list(s)
+        pos.sort()
+        print(docs[doc_id]['path'], pos)
