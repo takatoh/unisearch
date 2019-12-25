4e89e3f948924673e6d441992120009ef47c8a05
takatoh <takatoh.m@gmail.com>
2019-12-25 14:58:09 +0900

    create_index.py: Output doc data to docs.json file.

--- create_index.py
+++ create_index.py
@@ -9,6 +9,7 @@ DOC_DATA = 'docs.json'
 
 
 files = [os.path.join(DOC_DIR, f) for f in os.listdir(DOC_DIR)]
+docs = {}
 doc_id = 0
 for file in files:
     with open(file, 'r') as f:
@@ -16,5 +17,8 @@ for file in files:
         tokens = document.tokenize(text)
         index = document.classify(tokens)
         document.save_index(index, doc_id, INDEX_DIR)
+    docs[str(doc_id)] = {'name': os.path.basename(file), 'path': file}
     doc_id += 1
 
+with open(DOC_DATA, 'w') as f:
+    json.dump(docs, f, indent=2)
