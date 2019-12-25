22360b55e01b560b97d54099e5202404ba1d7177
takatoh <takatoh.m@gmail.com>
2019-12-25 14:44:55 +0900

    create_index.py: Save index.

--- create_index.py
+++ create_index.py
@@ -9,9 +9,12 @@ DOC_DATA = 'docs.json'
 
 
 files = [os.path.join(DOC_DIR, f) for f in os.listdir(DOC_DIR)]
+doc_id = 0
 for file in files:
     with open(file, 'r') as f:
         text = f.read()
         tokens = document.tokenize(text)
         index = document.classify(tokens)
-        print(index)
+        document.save_index(index, doc_id, INDEX_DIR)
+    doc_id += 1
+
