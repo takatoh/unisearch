ead93d494b3e718e03b91486925d7ff9688159fc
takatoh <takatoh.m@gmail.com>
2019-12-25 14:36:01 +0900

    unigram/document.py(save_index): New.

--- unigram/document.py
+++ unigram/document.py
@@ -1,5 +1,9 @@
+import os
+
+
 def tokenize(text):
     return list(text)
+
 
 def classify(token_list):
     tokens = {}
@@ -10,3 +14,10 @@ def classify(token_list):
         tokens[t].append(pos)
         pos += 1
     return tokens
+
+
+def save_index(index, doc_id, index_dir):
+    for c, idx in index.items():
+        l = str(doc_id) + ':' + ','.join(list(map(lambda x: str(x), idx))) + '\n'
+        with open(os.path.join(index_dir, str(ord(c)) + '.index'), 'w') as f:
+            f.write(l)
